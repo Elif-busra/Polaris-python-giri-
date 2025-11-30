0d18fc4e8319856ba049d59c3487eeda4e84a7cb
Elif-busra <elifbusraaarik@gmail.com>
2025-11-30 20:44:54 +0300

    Add age and year calculation for 100th birthday

--- 100yasina geleceğin yıl.py
+++ 100yasina geleceğin yıl.py
@@ -1,1 +1,9 @@
 
+isim = input ("Adınızı giriniz: ")
+yas = int(input("Yaşınızı giriniz: "))
+yil= int (input("Şu an hangi yıldasınız: "))
+fark= 100 - yas
+yashesap = (yil + fark , "yılında 100 yaşında olacaksınız.")
+
+
+print( yashesap )
